798b9a0e7e45bda4f97f2229dc87ceb80aae4b4e
pulkit1057 <100367696+pulkit1057@users.noreply.github.com>
2024-11-23 17:02:26 +0530

    final commit

--- app.js
+++ app.js
@@ -7,6 +7,34 @@ const doxctopdf = require('docx-pdf')
 
 const app = express()
 
+// function encryptPDF(inputFilePath, outputFilePath, password) {
+//     // Create the qpdf command to add password protection
+//     const command = `qpdf --encrypt ${password} ${password} 256 -- ${inputFilePath} ${outputFilePath}`;
+  
+//     // Execute the command
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`exec error: ${error}`);
+//         return;
+//       }
+//       if (stderr) {
+//         console.error(`stderr: ${stderr}`);
+//         return;
+//       }
+//       console.log(`PDF encrypted and saved to ${outputFilePath}`);
+//     });
+//   }
+  
+//   // Example usage
+//   const inputPDF = path.join(__dirname, 'input.pdf'); // Path to input PDF
+//   const outputPDF = path.join(__dirname, 'encrypted_output.pdf'); // Path to save encrypted PDF
+//   const password = 'your-password-here'; // Password for encryption
+  
+//   encryptPDF(inputPDF, outputPDF, password);
+
+
+
+
 // app.use(express.static('public'))
 app.use(express.static('uploads'))
 app.get('/', (req, res) => {
@@ -30,15 +58,13 @@ app.use(bodyparser.json())
 
 
 app.post("/docxtopdf", upload.single('file'), (req, res) => {
-    // const metaData = {
-    //     name : 
-    // }
+    
     const password = req.body.password;
     console.log(password)
     let ofp = Date.now() + "output.pdf";
 
 
-    doxctopdf(req.file.path, ofp,(err, result) => {
+    doxctopdf(req.file.path, ofp,async (err, result) => {
         if (err) {
             console.log(err)
         }
